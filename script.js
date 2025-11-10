9222cb4216c4afa5718dbb0055913cd62699b632
Marco Bacci <marcobacci94@gmail.com>
2025-11-10 23:15:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// question 1
+const hamburger = { name: "Cheese Burger", weight: 250 };
+const secondBurger = hamburger;
+secondBurger.name = 'Double Cheese Burger';
+secondBurger.weight = 500;
+
+console.log(hamburger.name); // Double Cheese Burger
+console.log(secondBurger.name ); // Double Cheese Burger
+// é stato creato un oggetto 
+
