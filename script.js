ce4e051cab1536675e384854c3b6638222170a7b
Marco Bacci <marcobacci94@gmail.com>
2025-11-10 23:18:39 +0100

    Feat: question 2 completed

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-// question 1
-const hamburger = { name: "Cheese Burger", weight: 250 };
-const secondBurger = hamburger;
-secondBurger.name = 'Double Cheese Burger';
-secondBurger.weight = 500;
+// question 1 ---------------------------------------
+// const hamburger = { name: "Cheese Burger", weight: 250 };
+// const secondBurger = hamburger;
+// secondBurger.name = 'Double Cheese Burger';
+// secondBurger.weight = 500;
 
-console.log(hamburger.name); // Double Cheese Burger
-console.log(secondBurger.name ); // Double Cheese Burger
-// é stato creato un oggetto 
+// console.log(hamburger.name); // Double Cheese Burger
+// console.log(secondBurger.name ); // Double Cheese Burger
+// // é stato creato un oggetto 
 
+// question 2 ---------------------------------------
+const hamburger = { 
+	name: "Cheese Burger", 
+	weight: 250,
+	ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
+};
+
+const secondBurger = {...hamburger};
+secondBurger.ingredients[0] = "Salad";
+
+console.log(hamburger.ingredients[0]); // Salad
+console.log(secondBurger.ingredients[0]); // Salad
+//  sono stati creati 3 oggetti
