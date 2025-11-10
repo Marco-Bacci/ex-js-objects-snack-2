d631b42037aadfa47ca0315018404b442bc14057
Marco Bacci <marcobacci94@gmail.com>
2025-11-10 23:26:16 +0100

    Feat: snack 4 completed

--- script.js
+++ script.js
@@ -25,21 +25,43 @@
 
 // question 3 ---------------------------------------
 
-const hamburger = { 
-	name: "Cheese Burger", 
-	weight: 250,
-	maker: {
-		name: "Anonymous Chef",
-		restaurant: {
-			name: "Hyur's Burgers",
-			address: "Main Street, 123",
-			isOpen: true,
-		},
-		age: 29
-	}
+// const hamburger = { 
+// 	name: "Cheese Burger", 
+// 	weight: 250,
+// 	maker: {
+// 		name: "Anonymous Chef",
+// 		restaurant: {
+// 			name: "Hyur's Burgers",
+// 			address: "Main Street, 123",
+// 			isOpen: true,
+// 		},
+// 		age: 29
+// 	}
+// };
+
+// const secondBurger = structuredClone(hamburger);
+// const thirdBurger = structuredClone(hamburger);
+// //  sono stati creati 9 oggetti
+
+// question 4 ---------------------------------------
+
+const chef = {
+	name: "Chef Hyur",
+	age: 29,
+	makeBurger: (num = 1) => {
+		console.log(`Ecco ${num} hamburger per te!`);
+	},
+}
+
+const restaurant = {
+	name: "Hyur's Burgers",
+	address: {
+		street: 'Main Street',
+		number: 123,
+	},
+	openingDate: new Date(2025, 3, 11),
+	isOpen: false,
 };
 
-const secondBurger = structuredClone(hamburger);
-const thirdBurger = structuredClone(hamburger);
-//  sono stati creati 9 oggetti
-
+// il miglior metodo per colonare l'oggetto chef è con spread operator, riscrivendo makeBurger perchè è una funzione
+// il miglior metodo per colonare l'restaurant è structuredClone perchè contiene oggetti annidati e anche un dato speciale (new Date)
